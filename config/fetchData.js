c623c393eec1a4d26e091f16a00de6d78cb27308
DevWebZone <Devanshu.Wadhwani@gmail.com>
2025-03-02 03:31:49 +0530

    update env variables

--- config/fetchData.js
+++ config/fetchData.js
@@ -1,12 +1,12 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 
-const genAI = new GoogleGenerativeAI("AIzaSyDNp5JU9vM99Z2QdW6rWl1_pz79oNHwqD8");
+const genAI = new GoogleGenerativeAI(process.env.GENERATIVE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-const prompt = "generate a dataset for 50 destinations which include clues, fun facts for each destination as per the below json format: [{\"city\":\"Paris\",\"country\":\"France\",\"clues\":[\"Thiscityishometoafamoustowerthatsparkleseverynight.\",\"Knownasthe'CityofLove'andahubforfashionandart.\"],\"fun_fact\":[\"TheEiffelTowerwassupposedtobedismantledafter20yearsbutwassavedbecauseitwasusefulforradiotransmissions!\",\"Parishasonlyonestopsignintheentirecity—mostintersectionsrelyonpriority-to-the-rightrules.\"],\"trivia\":[\"Thiscityisfamousforitscroissantsandmacarons.Bonappétit!\",\"PariswasoriginallyaRomancitycalledLutetia.\"]}]";
-
+const prompt = process.env.PROMPT;
 module.exports.getDestinationData = async function() {
   try {
     
