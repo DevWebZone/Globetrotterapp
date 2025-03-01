776b411702f09846e3902469cc81c4667a63663b
DevWebZone <Devanshu.Wadhwani@gmail.com>
2025-03-02 03:51:00 +0530

    remove .env file and updated db url

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/User_db');
+
+require('dotenv').config();
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 
